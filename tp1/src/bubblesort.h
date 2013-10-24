85e93e6722a927cb57672d7aa404fbb0fb924463
rburdet <rodrigoburdet@gmail.com>
2013-10-24 02:59:34 -0300

    cambios pocos

--- tp1/src/bubblesort.h
+++ tp1/src/bubblesort.h
@@ -1,6 +1,9 @@
 #ifndef __BUBBLESORT_H__
 #define __BUBBLESORT_H__
+
 #include <stdio.h>
+
+typedef int (*TCompareFunction)(void*, void*);
 
 void bubblesort(void** array, size_t size, TCompareFunction cmp);
 
